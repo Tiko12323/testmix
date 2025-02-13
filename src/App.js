27e6abc0bc5b4698d7db646b268239da6d7477fd
Tiko12323 <tikomartirosyan15@gmail.com>
2025-02-14 00:07:15 +0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routesConfig from "./routes/routes";
+
+const router = createBrowserRouter(routesConfig)
 
 function App() {
-  const [task, setTask]= useState('')
-  const [newTask, setNewTask] = useState('')
-  const [arr, setArr] = useState([
-    {id:1, title: 'vahe'},
-    {id:2, title: 'tiko'}
-  ])
-
-  function add () {
-    setArr([
-      ...arr,
-      {id: Math.random(), title: task}
-    ])
-  }
-
-  function onDelete(id){
-    setArr(arr.filter((item)=> id !== item.id))
-  }
-
-  function onEdit(id){
-    setArr(arr.map((item)=> {
-      if(item.id === id){
-        item.title = newTask
-      }
-      return item
-    }))
-
-  }
   return (
-    <div>
-      <p>todo list</p>
-      <input onChange={(e)=>setTask(e.target.value)}/>
-      <button onClick={add}>add</button>
-      {
-        arr.map((item)=>(
-          <div key={item.id}>
-            <input onChange={(e)=>setNewTask(e.target.value)} defaultValue={item.title}/>
-            <button onClick={()=>onDelete(item.id)}>delete</button>
-            <button onClick={()=>onEdit(item.id)}>edit</button>
-          </div>
-        ))
-      }
+    <>
+    <RouterProvider router={router}/>
       
-    </div>
+    </>
+    
   );
 }
 
---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+import DefaultLayOut from "../layout/DefaultLayOut"
+import OutLayOut from "../layout/OutLayOut"
+
+const routesConfig = [
+    {
+        element: <DefaultLayOut />,
+        children: []
+    },
+    {
+        element: <OutLayOut />,
+        children: []
+    }
+]
